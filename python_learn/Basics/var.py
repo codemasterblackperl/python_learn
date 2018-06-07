509bb190730e0cdbd651e55624276832a21e144d
Ajit <codemasterblackperl@outlook.com>
2018-06-07 23:00:28 +0530

    variables basics

---
+++ python_learn/Basics/var.py
@@ -0,0 +1,32 @@
+
+i=10
+print(i)
+i+=1
+print(i)
+
+s='''this is multi line string
+using 3 quotes'''
+print(s)
+
+#python uses physical line and logical line and each logical line is same as physical line however you can add more than 1 logical line to physical line
+i=5
+print(i)
+
+#is same as
+
+i=5;
+print(i);
+
+#is same as
+
+i=6;print(i);
+
+#is same as 
+
+i=7;print(i)
+
+#is same as
+i=\
+5;print(i)
+
+#also remember space at the begining of the line is very important. It is same as brackets in c#
