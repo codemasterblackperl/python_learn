46915d66b48820f97af2d35ed2aee7b190169fe7
Ajit <codemasterblackperl@outlook.com>
2018-06-08 09:34:27 +0530

    basic expression and operators is done

---
+++ python_learn/Basics/operators_expressions.py
@@ -0,0 +1,48 @@
+print(2+3)
+print('la'*3)
+
+#x to the power of y
+print(3**4)
+
+print(13/3)
+
+#divide and floor
+print(13//3)
+
+#left shift
+print(2<<2) #is 8 because use binary number
+
+#right shift
+print(11>1) #is 5
+
+print(5<3)
+
+print('Ajit'=='ajit')
+
+#boolean
+x=5;y=5;
+print(x and y)
+
+print(not True)
+
+a="Ajit"
+print('f' in a)
+
+#  order of execution from lowest to highest priority
+
+#lambda : Lambda Expression
+#if - else : Conditional expression
+#or : Boolean OR
+#and : Boolean AND
+#not x : Boolean NOT
+#in, not in, is, is not, <, <=, >, >=, !=, == : Comparisons, including membership tests and identity tests
+#| : Bitwise OR
+#^ : Bitwise XOR
+#& : Bitwise AND
+#<<, >> : Shifts
+#+, - : Addition and subtraction
+#*, /, //, % : Multiplication, Division, Floor Division and Remainder
+#+x, -x, ~x : Positive, Negative, bitwise NOT
+#** : Exponentiation
+#x[index], x[index:index], x(arguments...), x.attribute : Subscription, slicing, call, attribute reference
+#(expressions...), [expressions...], {key: value...}, {expressions...} : Binding or tuple display, list display, dictionary display, set display
