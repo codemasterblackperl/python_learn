772d3d4c4aa628efd9330ef00597b29d61263492
Ajit <codemasterblackperl@outlook.com>
2018-06-08 18:36:14 +0530

    modules almost complete.

--- python_learn/Modules/module_test.py
+++ python_learn/Modules/module_test.py
@@ -5,3 +5,22 @@ result=add_module.add_numbers(1,2,3,4)
 print("Result: ",result)
 
 print(add_module.__version__)
+
+print(dir(add_module))
+
+# __init__.py will be used to tell analyzer that this folder is special because it contains Python modules.
+# folder structure
+
+#<some folder present in the sys.path>/
+#    - world/
+#        - __init__.py
+#        - asia/
+#            - __init__.py
+#            - india/
+#                - __init__.py
+#                - foo.py
+#        - africa/
+#            - __init__.py
+#            - madagascar/
+#                - __init__.py
+#                - bar.py
